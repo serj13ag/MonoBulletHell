84559b08ae1be3159dda60e44806f885424e290d
Serj Agafonov <86243008+serj13ag@users.noreply.github.com>
2026-04-12 21:36:40 +0300

    add flash effect shader

--- MonoBulletHell/Gameplay/Services/ContentService.cs
+++ MonoBulletHell/Gameplay/Services/ContentService.cs
@@ -1,4 +1,5 @@
 using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 using MonoBulletHell.Core.Graphics;
 
 namespace MonoBulletHell.Gameplay.Services;
@@ -8,19 +9,28 @@ public interface IContentService
     void Load(ContentManager content);
 
     Sprite CreateSprite(string spriteName);
+    Effect GetFlashEffect();
 }
 
 public class ContentService : IContentService
 {
     private TextureAtlas _atlas;
+    private Effect _flashEffect;
 
     public void Load(ContentManager content)
     {
         _atlas = TextureAtlas.FromFile(content, "images/atlas-definition.json");
+
+        _flashEffect = content.Load<Effect>("shaders/flashEffect");
     }
 
     public Sprite CreateSprite(string spriteName)
     {
         return _atlas.CreateSprite(spriteName);
+    }
+
+    public Effect GetFlashEffect()
+    {
+        return _flashEffect;
     }
 }
