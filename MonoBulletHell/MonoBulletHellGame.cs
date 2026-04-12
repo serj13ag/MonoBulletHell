787ba30855ca7c4d90678b2425fdeb9c38209a8c
Serj Agafonov <86243008+serj13ag@users.noreply.github.com>
2026-04-12 14:23:32 +0300

    implement scaling

--- MonoBulletHell/MonoBulletHellGame.cs
+++ MonoBulletHell/MonoBulletHellGame.cs
@@ -1,6 +1,7 @@
 ﻿using Gum.Forms.Controls;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using MonoBulletHell.Scenes;
 using MonoBulletHell.Services;
@@ -10,19 +11,26 @@ namespace MonoBulletHell;
 
 public class MonoBulletHellGame : Game
 {
+    private const int ScreenScale = 1;
+
     private readonly CompositionRoot _root;
 
+    private SpriteBatch _spriteBatch;
+    private GumService _gumService;
     private IInputService _inputService;
     private ISceneService _sceneService;
     private IDebugService _debugService;
+
+    private RenderTarget2D _nativeRenderTarget;
+    private Rectangle _actualScreenRectangle;
 
     public MonoBulletHellGame()
     {
         _root = new CompositionRoot(this);
 
         var graphics = new GraphicsDeviceManager(this);
-        graphics.PreferredBackBufferWidth = Constants.VirtualWidth;
-        graphics.PreferredBackBufferHeight = Constants.VirtualHeight;
+        graphics.PreferredBackBufferWidth = Constants.VirtualWidth * ScreenScale;
+        graphics.PreferredBackBufferHeight = Constants.VirtualHeight * ScreenScale;
         graphics.IsFullScreen = false;
 
         Content.RootDirectory = "Content";
@@ -36,11 +44,16 @@ public class MonoBulletHellGame : Game
 
         _root.Initialize(Content, GraphicsDevice);
 
+        _spriteBatch = _root.GetInstance<SpriteBatch>();
+        _gumService = _root.GetInstance<GumService>();
         _inputService = _root.GetInstance<IInputService>();
         _sceneService = _root.GetInstance<ISceneService>();
         _debugService = _root.GetInstance<IDebugService>();
 
         InitializeGum(Content);
+
+        _nativeRenderTarget = new RenderTarget2D(GraphicsDevice, Constants.VirtualWidth, Constants.VirtualHeight);
+        _actualScreenRectangle = new Rectangle(0, 0, Constants.VirtualWidth * ScreenScale, Constants.VirtualHeight * ScreenScale);
 
         _sceneService.ChangeScene(SceneType.Title);
     }
@@ -50,6 +63,8 @@ public class MonoBulletHellGame : Game
         _inputService.Update();
         _debugService.Update();
 
+        UpdateGumCursorTransform();
+
         _sceneService.Update(gameTime);
 
         base.Update(gameTime);
@@ -57,25 +72,44 @@ public class MonoBulletHellGame : Game
 
     protected override void Draw(GameTime gameTime)
     {
+        GraphicsDevice.SetRenderTarget(_nativeRenderTarget);
+
         _sceneService.Draw(gameTime);
         _debugService.Render();
+
+        // Scale image
+        GraphicsDevice.SetRenderTarget(null);
+        _spriteBatch.Begin(samplerState: SamplerState.PointClamp);
+        _spriteBatch.Draw(_nativeRenderTarget, _actualScreenRectangle, Color.White);
+        _spriteBatch.End();
 
         base.Draw(gameTime);
     }
 
     private void InitializeGum(ContentManager content)
     {
-        var gumService = _root.GetInstance<GumService>();
+        _gumService.Initialize(this);
+        _gumService.ContentLoader.XnaContentManager = content;
 
-        gumService.Initialize(this);
-        gumService.ContentLoader.XnaContentManager = content;
-
-        FrameworkElement.KeyboardsForUiControl.Add(gumService.Keyboard);
-        FrameworkElement.GamePadsForUiControl.AddRange(gumService.Gamepads);
+        FrameworkElement.KeyboardsForUiControl.Add(_gumService.Keyboard);
+        FrameworkElement.GamePadsForUiControl.AddRange(_gumService.Gamepads);
         FrameworkElement.TabReverseKeyCombos.Add(new KeyCombo() { PushedKey = Keys.Up });
         FrameworkElement.TabKeyCombos.Add(new KeyCombo() { PushedKey = Keys.Down });
 
-        gumService.CanvasWidth = GraphicsDevice.PresentationParameters.BackBufferWidth;
-        gumService.CanvasHeight = GraphicsDevice.PresentationParameters.BackBufferHeight;
+        _gumService.CanvasWidth = Constants.VirtualWidth;
+        _gumService.CanvasHeight = Constants.VirtualHeight;
+    }
+
+    private void UpdateGumCursorTransform()
+    {
+        var translation = Matrix.CreateTranslation(-_actualScreenRectangle.X, -_actualScreenRectangle.Y, 0);
+
+        var xScale = Constants.VirtualWidth / (float)_actualScreenRectangle.Width;
+        var yScale = Constants.VirtualHeight / (float)_actualScreenRectangle.Height;
+        var scale = Matrix.CreateScale(xScale, yScale, 1f);
+
+        var transform = translation * scale;
+
+        _gumService.Cursor.TransformMatrix = transform;
     }
 }
