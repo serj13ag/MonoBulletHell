a1a4aeca187fa1d63257932f20a20aa6b8f71376
Serj Agafonov <86243008+serj13ag@users.noreply.github.com>
2026-04-19 11:35:06 +0300

    add comments

--- MonoBulletHell/Data/FormationData.cs
+++ MonoBulletHell/Data/FormationData.cs
@@ -6,11 +6,12 @@ namespace MonoBulletHell.Data;
 public class FormationData
 {
     public FormationType Type { get; set; }
-    public int Count { get; set; }
     public float Rotation { get; set; }
 
+    public int Count { get; set; } // Line, Circle
     public int Spacing { get; set; } // Line
     public float Radius { get; set; } // Circle
+
     public int Rows { get; set; } // Grid, VShape
     public int Columns { get; set; } // Grid
     public float SpacingX { get; set; } // Grid, VShape
