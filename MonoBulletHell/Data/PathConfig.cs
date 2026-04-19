6098e05ab4002bfae3ffc3d870b08a92fc2b35e7
Serj Agafonov <86243008+serj13ag@users.noreply.github.com>
2026-04-19 12:04:22 +0300

    add starting level spawn data

--- MonoBulletHell/Data/PathConfig.cs
+++ MonoBulletHell/Data/PathConfig.cs
@@ -16,6 +16,7 @@ public class PathData
     public string Name { get; set; }
     public int Loops { get; set; }
     public List<PathPointData> Points { get; set; }
+    // TODO: add inverse bool
 }
 
 [Serializable]
