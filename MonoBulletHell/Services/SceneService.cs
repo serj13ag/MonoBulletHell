c520e321057f72a668e47f6324ec211b0a011bf6
Serj Agafonov <86243008+serj13ag@users.noreply.github.com>
2026-05-07 20:45:34 +0300

    remove GC.Collect() call

--- MonoBulletHell/Services/SceneService.cs
+++ MonoBulletHell/Services/SceneService.cs
@@ -59,8 +59,6 @@ public class SceneService : ISceneService
 
         _sceneScope?.Dispose();
 
-        GC.Collect();
-
         _sceneScope = _scopeFactory.Invoke();
 
         _activeScene = GetNextScene(nextSceneType, _sceneScope);
